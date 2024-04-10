cd7995e6336033c9a4d14f418418053e70ca3ac7
Mateus <125222236+Mateus-Allebrand@users.noreply.github.com>
2024-04-10 16:55:15 -0300

    create test.py

---
+++ restaurante/app.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json"
+
+response = requests.get(url)
+
+print(response)
+
+
+if response.status_code == 200:
+    dados_json = response.json() 
+    print(dados_json)
+else:
+    print(f"O Erro foi {response.status_code}")
